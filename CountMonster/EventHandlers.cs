9fcea8c40391370093800f4179f99fc6b9bf35c4
John Sedlak <john.sedlak@outlook.com>
2021-12-21 10:30:28 -0500

    Adds inline editing to counter names

---
+++ CountMonster/EventHandlers.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace CountMonster
+{
+    [EventHandler("onmouseleave", typeof(MouseEventArgs), true, true)]
+    [EventHandler("onmouseenter", typeof(MouseEventArgs), true, true)]
+    public static class EventHandlers { }
+}
